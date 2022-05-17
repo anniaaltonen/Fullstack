0b6f13182b79a9a2ba89cdc21f7e85f7f2e8b38b
anniaaltonen <102946659+anniaaltonen@users.noreply.github.com>
2022-05-17 13:01:21 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,85 +1,41 @@
 import { useState } from 'react'
 
-const Header = ({ title }) => {
+const Yhteystieto = ({ key, yhteystieto }) => {
+  const nimi = yhteystieto;
+  const kuka = key;
   return (
-    <div>
-      <h1>{title}</h1>
-    </div>
-  )
-}
-
-const StatisticLine = ({ text, value }) => {
-  const nimi = text
-  const arvo = value
-
-  return (
-    <div>
-      <p>{nimi} {arvo}</p>
-    </div>
-  )
-}
-
-const Statistics = ({ good, neutral, bad }) => {
-  const hyva = good;
-  const neutraali = neutral;
-  const paha = bad;
-  console.log('bad', bad)
-  const all = (hyva[0] + neutraali[0] + paha[0]);
-  console.log('all', all)
-  const arvosanaHyva = hyva[0];
-  const arvosanaNeutraali = 0;
-  const arvoSanaPaha = (paha[0] * -1)
-  const average = ((arvosanaHyva + arvoSanaPaha + arvosanaNeutraali) / all);
-  const positive = (hyva[0] / all);
-
-  if (all >= 1) {
-
-    return (
-      <div>
-        <h2>tilastot</h2>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutraali} />
-        <StatisticLine text="bad" value={paha} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive} />
-      </div>
-    )
-  } else {
-
-    return (
-      <div>
-        <h2>tilastot</h2>
-        <p>nothing to display, ok</p>
-      </div>
-    )
-  }
-}
-
-const Button = (props) => {
-
-  return (
-    <div>
-      <button onClick={props.handleClick}>{props.name}</button>
-    </div>
+    <p>palautus komponentista Yhteystieto{nimi}</p>
   )
 }
 
 const App = () => {
-  const title = 'give feedback';
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
 
   return (
     <div>
-      <Header title={title} />
-      <Button handleClick={() => setGood(good + 1)} name="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} name="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} name="bad" />
-      <Statistics good={[good, setGood]} neutral={[neutral, setNeutral]} bad={[bad, setBad]} />
+      <h2>Phonebook</h2>
+      <form onSubmit={setPersons}>
+        <div>
+          name: <input
+            value={newName}
+            onChange={setNewName}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul class="yhteystieto">
+        {persons.map(yhteystieto =>
+          <Yhteystieto key={yhteystieto.id} yhteystieto={yhteystieto} />
+        )}
+      </ul>
     </div>
   )
-}
 
-export default App
+}
+export default App;
